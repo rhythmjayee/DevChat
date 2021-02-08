cee946ad749c044d78287566afc66f43d414b5d4
rhythm jayee <jayeerythm8@gmail.com>
2021-02-08 22:30:31 +0530

    register form validate

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -9,34 +9,73 @@ const Register=()=>{
         username:'',
         email:'',
         password:'',
-        passwordConfirmation:''
+        passwordConfirmation:'',
+        errorsState:[],
+        loading:false
     });
-    const {username,email,password,passwordConfirmation}=state;
+    const {username,email,password,passwordConfirmation,errorsState,loading}=state;
+
+    const isFormValid=()=>{
+        setstate({...state,errorsState:[],loading:true});
+        let errors=[];
+        let error;
+        if(isFormEmpty(state)){
+            error={message:"Fill in all fields"};
+            errors=[...errors,error];
+            setstate({...state,errorsState:errors});
+            return false;
+        }else if(!isPasswordValid(state)){
+            error={message:"Password is invalid"};
+            errors=[...errors,error];
+            setstate({...state,errorsState:errors});
+            return false;
+        }else{
+            return true;
+        }
+    }
+    const isFormEmpty=({username,email,password,passwordConfirmation})=>{
+        return !username.length || !email.length || !password.length || !passwordConfirmation.length;
+    }
+    const isPasswordValid=({password,passwordConfirmation})=>{
+        if(password.length<6 || passwordConfirmation<6){
+            return false;
+        }else if(password!==passwordConfirmation){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    const displayErrors=()=>( errorsState.map((err,i)=><p key={i}>{err.message}</p>))
+
 
     const handleChange=(e)=>{
         setstate({...state,[e.target.name]:e.target.value});
     };
     const handleSubmit=(e)=>{
-        e.preventDefault();
-        console.log(state.email,state.password);
-        firebase
-        .auth()
-        .createUserWithEmailAndPassword(email,password)
-        .then(createdUser=>{
-            console.log(createdUser);
-            setstate({
-                username:'',
-                email:'',
-                password:'',
-                passwordConfirmation:''
-            });
-        })
-        .catch(err=>{
-            console.error(err);
-        })
+        if(isFormValid()){
+            e.preventDefault();
+            console.log(state.email,state.password);
+            firebase
+            .auth()
+            .createUserWithEmailAndPassword(email,password)
+            .then(createdUser=>{
+                console.log(createdUser);
+                setstate({
+                    username:'',
+                    email:'',
+                    password:'',
+                    passwordConfirmation:'',
+                    errorsState:[],
+                    loading:false
+                });
+            })
+            .catch(err=>{
+                console.error(err);
+                setstate({...state,errorsState:[...errorsState,err], loading:false});
+            })
+        }
     }
-    // console.log(state);
-   
 
     return(
 
@@ -46,6 +85,13 @@ const Register=()=>{
                     <Icon name='code' color='pink'/>
                     Register for DevChat
                 </Header>
+                {errorsState.length>0 && (
+                    <Message error>
+                        <h3>
+                            {displayErrors()}
+                        </h3>
+                    </Message>
+                )}
                 <Form size='large' style={{background:'#E15597'}} onSubmit={handleSubmit}>
                 <Segment stacked style={{background:'#E15597'}}>
                     <Form.Input fluid name='username' icon='user' iconPosition='left'
@@ -61,7 +107,7 @@ const Register=()=>{
                     <Form.Input fluid name='passwordConfirmation' icon='repeat' iconPosition='left'
                     placeholder='Password Confirmation'  onChange={handleChange} value={state.passwordConfirmation} type='password' />
 
-                    <Button style={{background:'#282C35'}} fluid size='large'>
+                    <Button disabled={loading} className={loading?'loading':''} style={{background:'#282C35'}} fluid size='large'>
                         <p style={{color:'#E15597'}}>Submit</p>
                     </Button>
                 </Segment>
