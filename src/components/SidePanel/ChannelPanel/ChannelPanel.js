476a3e127cbc7613de192fa9cc52914f875a9dc5
rhythmjayee <jayeerythm8@gmail.com>
2021-02-09 15:35:29 +0530

    added modal for channels

--- src/components/SidePanel/ChannelPanel/ChannelPanel.js
+++ src/components/SidePanel/ChannelPanel/ChannelPanel.js
@@ -1,23 +1,60 @@
 import React,{useState} from 'react'
 
-import {Icon, Menu, MenuItem, MenuMenu} from 'semantic-ui-react';
+import {FormField, Icon, Input, Menu, MenuItem, MenuMenu,Form,Button, Modal, ModalActions, ModalContent, ModalHeader} from 'semantic-ui-react';
 
  const ChannelPanel = () => {
      const [state, setstate] = useState({
-         channels:[]
+         channels:[],
+         channelname:'',
+         channelDetails:'',
+         modal:false
      });
 
-     const {channels}=state;
+     const {channels,modal}=state;
+
+    //  const closeModal=()=>{
+    //     setstate({...state,modal:false});
+    //  }
+     const handleChange=(e)=>{
+        setstate({...state,[e.taregt.name]:e.target.value});
+     }
+     const modalHandler=()=>{
+        setstate({...state,modal:!modal});
+     }
     return (
+        <>
         <MenuMenu style={{marginTop:'1em',paddingBottom:'2em'}}>
             <MenuItem>
                 <span>
                     <Icon name='exchange'/>
                 </span>
-                {' '}CHANNELS({channels.length})  <Icon name='add'/>
+                {' '}CHANNELS({channels.length})  <Icon name='add' onClick={modalHandler}/>
             </MenuItem>
             {/* Channels */}
         </MenuMenu>
+        {/* Add channel modal */}
+        <Modal basic open={modal} onClose={modalHandler}>
+            <ModalHeader>Add a Channel</ModalHeader>
+            <ModalContent>
+                <Form>
+                    <FormField>
+                        <Input fluid label='Name of channel' name='channel' onChange={handleChange} />
+                    </FormField>
+                    <FormField>
+                        <Input fluid label='About the channel' name='channelDetails' onChange={handleChange} />
+                    </FormField>
+                </Form>
+            </ModalContent>
+            <ModalActions>
+                <Button color='green' inverted>
+                    <Icon name='checkmark'/> Add
+                </Button>
+                <Button color='red' inverted>
+                    <Icon name='remove' onClick={modalHandler}/> cancel
+                </Button>
+            </ModalActions>
+        </Modal>
+        </>
     )
 }
 
